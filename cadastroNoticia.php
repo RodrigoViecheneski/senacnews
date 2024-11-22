c77d55f85d4e10294042086654733cb0ca6ddc24
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2024-11-22 20:35:24 -0300

    commit 22-11

---
+++ cadastroNoticia.php
@@ -0,0 +1,7 @@
+<?php include 'inc/header.inc.php';?>
+
+<h1>Aqui será colocado minhas notícias!!</h1>
+
+
+
+<?php include 'inc/footer.inc.php';?>
---
+++ index.php
@@ -0,0 +1,86 @@
+<?php include 'inc/header.inc.php';?>
+
+        <div class="container">
+                <section class="busca">
+                        <h2>Busca</h2>
+                        <form>
+                                <input type="search">
+                                <button>Busca</button>
+                        </form>
+                </section>
+<!--Fim busca-->
+        <section class="menu-noticias">
+                <h2>Notícias</h2>
+                <nav>
+                        <ul>
+                                <li><a href="#">Esportes</a>
+                                        <ul>
+                                                <li><a href="#">Futebol</a></li>
+                                                <li><a href="#">Volei</a></li>
+                                                <li><a href="#">Basquete</a></li>
+                                        </ul>
+                                </li>
+                                <li><a href="#">Policial</a></li>
+                                <li><a href="#">Economia</a></li>
+                                <li><a href="#">Alunos</a></li>
+                                <li><a href="#">Eventos</a></li>
+                                <li><a href="#">Instituições</a></li>
+                                <li><a href="#">Biblioteca</a></li>
+                               <!-- <li><a href="#">Calendário</a></li>-->
+                        </ul>
+                </nav>
+        </section>
+        <section class="destaque">
+                        <img src="img/principal.jpg" alt="imagem de destaque">
+        </section>
+<!--Fim destaque-->
+        <div class="todo">
+                <section class="principal">
+                        <h2>Notícias</h2>
+                        <ul>
+                                <li>
+                                        <a href="#">
+                                                <figure>
+                                                        <img src="img/principal2.jpg" alt="Notícia do dia"/>
+                                                        <figcaption>Senac promove viagens do Ficthon a alunos vencedores.</figcaption>
+                                                </figure>
+                                        </a>
+                                        <p>
+                                        O ficthon é uma competição que ocorrerá na capital Curitiba, ela tem por objetivo mobilizar os estudantes a respeito das ODS, para isso, cada equipe precisava desenvolver uma idea-ação voltada para resolução de um problema que tivesse relação com as ODS.
+                                        </p>
+                                </li>
+                                <li>
+                                        <a href="#">
+                                                <figure>
+                                                        <img src="img/principal2.jpg" alt="Notícia do dia"/>
+                                                        <figcaption>Senac promove viagens do Ficthon a alunos vencedores.</figcaption>
+                                                </figure>
+                                        </a>
+                                </li>
+                                <li>
+                                        <a href="#">
+                                                <figure>
+                                                        <img src="img/principal2.jpg" alt="Notícia do dia"/>
+                                                        <figcaption>Senac promove viagens do Ficthon a alunos vencedores.</figcaption>
+                                                </figure>
+                                        </a>
+                                </li>
+                                <li>
+                                        <a href="#">
+                                                <figure>
+                                                        <img src="img/principal2.jpg" alt="Notícia do dia"/>
+                                                        <figcaption>Senac promove viagens do Ficthon a alunos vencedores.</figcaption>
+                                                </figure>
+                                        </a>
+                                </li>
+                        </ul>
+
+                </section>
+                <section class="feeds">
+
+                </section>
+        </div>
+        </div>
+
+
+<?php include 'inc/footer.inc.php';?>
